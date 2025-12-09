5a0bc0cadac64c42d150ade89f8521df64ca601b
Nailis7305 <nailis7305@gmail.com>
2025-12-08 19:08:22 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routes.export_router import router as export_router
-from app.routes import clientes_router
+from app.routes.clientes_router import router as clientes_router
 
 app = FastAPI()
 
@@ -9,5 +9,5 @@ def root():
     return {"message": "Python CSV Service running"}
 
 app.include_router(export_router, prefix="/api/clientes", tags=["Clientes"])
+app.include_router(clientes_router, prefix="/api/clientes", tags=["Clientes"])
 
-app.include_router(clientes_router.router, prefix="/api/clientes", tags=["Clientes"])
